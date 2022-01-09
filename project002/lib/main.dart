3c589d61877ec270c292b321a6f4553edaee1693
Diyar Ceran <yakupceran00@gmail.com>
2022-01-09 10:34:49 +0100

    wigets

--- project002/lib/main.dart
+++ project002/lib/main.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'Material App',
         home: HomePage());
@@ -27,15 +27,39 @@ class HomePage extends StatelessWidget {
         elevation: 4,
         centerTitle: true,
         title: const Text("Uber"),
-        leading: Container(
-          child: const Text(
-            "B",
-            style: TextStyle(color: Colors.black),
+        leading: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Container(
+            child: const Center(
+              child: Text(
+                "B",
+                style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.w500,
+                    fontSize: 18),
+              ),
+            ),
+            color: Colors.white,
           ),
-          color: Colors.white,
+        ),
+        actions: const [
+          Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Icon(Icons.supervised_user_circle_outlined),
+          )
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: const [
+            Text(
+              "Hoşgeldiniz,",
+              style: TextStyle(fontSize: 30.0, fontWeight: FontWeight.w700),
+            ),
+          ],
         ),
       ),
-      body: Column(),
     );
   }
 }
