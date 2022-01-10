d093ff0f18dce9c25f1415ddbcafcb744c49f6af
Diyar Ceran <yakupceran00@gmail.com>
2022-01-10 11:34:32 +0100

    last changes

--- project002/lib/main.dart
+++ project002/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
@@ -10,12 +12,12 @@ class MyApp extends StatelessWidget {
     return const MaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'Material App',
-        home: HomePage());
+        home: HomePageWidget());
   }
 }
 
-class HomePage extends StatelessWidget {
-  const HomePage({
+class HomePageWidget extends StatelessWidget {
+  const HomePageWidget({
     Key? key,
   }) : super(key: key);
 
@@ -39,73 +41,111 @@ class HomePageBody extends StatelessWidget {
       padding: const EdgeInsets.all(16.0),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          const Padding(
+        children: const [
+          Padding(
             padding: EdgeInsets.only(bottom: 8.0),
             child: Text(
               "Hoşgeldiniz,",
               style: TextStyle(fontSize: 30.0, fontWeight: FontWeight.w700),
             ),
           ),
-          Row(
-            children: [
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.only(right: 16.0),
-                  child: Container(
-                      height: 50,
-                      color: Colors.blueGrey.withOpacity(0.35),
-                      child: const Align(
-                          alignment: Alignment.centerLeft,
-                          child: Padding(
-                            padding: EdgeInsets.only(left: 8),
-                            child: Text("Ara"),
-                          ))),
-                ),
-              ),
-              const Icon(Icons.search),
-            ],
+          MainPageSearchAreaWidget(),
+          SizedBox(
+            height: 16,
           ),
-          const SizedBox(
-            height: 16.0,
-          ),
-          const Text(
-            "Geçmiş yolculuklarınız",
+          Text(
+            "Geçmiş Yolculuklarınız",
             style: TextStyle(fontWeight: FontWeight.w600),
           ),
-          const SizedBox(height: 8.0),
-          Container(
-            decoration: BoxDecoration(
-                color: Colors.black, borderRadius: BorderRadius.circular(10)),
-            child: Padding(
-              padding: const EdgeInsets.all(12.0),
-              child: Row(
-                children: const [
-                  Icon(
-                    Icons.local_taxi_rounded,
-                    color: Colors.white,
-                  ),
-                  SizedBox(
-                    width: 8.0,
-                  ),
-                  Expanded(
-                    child: Text(
-                      "Kadıköy - Ataşehir",
-                      style: TextStyle(
-                          color: Colors.white, fontWeight: FontWeight.w500),
-                    ),
-                  ),
-                  Text(
-                    "56,90₺",
-                    style: TextStyle(
-                        color: Colors.green, fontWeight: FontWeight.bold),
-                  )
-                ],
-              ),
-            ),
-          )
+          SizedBox(
+            height: 8.0,
+          ),
+          CardLocationWidget(),
+          SizedBox(height: 8.0),
+          CardLocationWidget(),
+          SizedBox(height: 8.0),
+          CardLocationWidget(),
+          SizedBox(height: 8.0),
+          CardLocationWidget(),
+          SizedBox(height: 8.0),
+          CardLocationWidget(),
+          SizedBox(height: 8.0),
+          CardLocationWidget(),
+          SizedBox(height: 8.0),
+          CardLocationWidget(),
         ],
       ),
+    );
+  }
+}
+
+class CardLocationWidget extends StatelessWidget {
+  const CardLocationWidget({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          color: Colors.black, borderRadius: BorderRadius.circular(10)),
+      child: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Row(
+          children: const [
+            Icon(
+              Icons.local_taxi_rounded,
+              color: Colors.white,
+            ),
+            SizedBox(
+              width: 8.0,
+            ),
+            Expanded(
+              child: Text(
+                "KadıKöy - Ataşehir",
+                style:
+                    TextStyle(color: Colors.white, fontWeight: FontWeight.w500),
+              ),
+            ),
+            Text(
+              "58,80₺",
+              style:
+                  TextStyle(color: Colors.green, fontWeight: FontWeight.bold),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class MainPageSearchAreaWidget extends StatelessWidget {
+  const MainPageSearchAreaWidget({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.only(right: 16.0),
+            child: Container(
+              height: 50,
+              color: Colors.blueGrey.withOpacity(0.35),
+              child: const Align(
+                alignment: Alignment.centerLeft,
+                child: Padding(
+                  padding: EdgeInsets.only(left: 8.0),
+                  child: Text("Ara"),
+                ),
+              ),
+            ),
+          ),
+        ),
+        const Icon(Icons.search)
+      ],
     );
   }
 }
@@ -113,19 +153,18 @@ class HomePageBody extends StatelessWidget {
 class MyAppBar extends AppBar {
   MyAppBar({Key? key})
       : super(
-          key: key,
-          backgroundColor: Colors.black,
-          elevation: 4,
-          centerTitle: true,
-          title: const Text("Uber"),
-          leading: const MyAppBarLeading(),
-          actions: const [
-            Padding(
-              padding: EdgeInsets.all(8.0),
-              child: Icon(Icons.supervised_user_circle_outlined),
-            )
-          ],
-        );
+            key: key,
+            backgroundColor: Colors.black,
+            elevation: 4,
+            centerTitle: true,
+            leading: const MyAppBarLeading(),
+            title: const Text("Uber"),
+            actions: const [
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Icon(Icons.supervised_user_circle_rounded),
+              )
+            ]);
 }
 
 class MyAppBarLeading extends StatelessWidget {
@@ -138,6 +177,7 @@ class MyAppBarLeading extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: Container(
+        color: Colors.white,
         child: const Center(
           child: Text(
             "B",
@@ -145,7 +185,6 @@ class MyAppBarLeading extends StatelessWidget {
                 color: Colors.black, fontWeight: FontWeight.w500, fontSize: 18),
           ),
         ),
-        color: Colors.white,
       ),
     );
   }
